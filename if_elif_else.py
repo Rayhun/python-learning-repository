467f6fd8bfbac6ec36d655d6ea14a8185f553cb0
rayhan <rayhunkhan27@gmail.com>
2020-09-09 23:00:09 +0600

    if elif else ststement

---
+++ if_elif_else.py
@@ -0,0 +1,43 @@
+name = 'Rayhan'
+
+# if statement 
+
+'''
+multi 
+line 
+comment 
+'''
+
+# if name == 'Rayhan':
+# 	print('True')
+# 	print('True')
+# 	print('True')
+# 	print('True')
+# 	print('True') 
+
+
+# if else statement
+# if name == 'Rayan':
+# 	print('True')
+# 	print('True')
+# 	print('True')
+# 	print('True')  
+# else:
+# 	print('No')
+
+bangla = 47
+
+if bangla >= 80:
+	print('A+')
+elif bangla >= 70:
+	print('A')
+elif bangla >= 60:
+	print('B')
+elif bangla >= 48:
+	print('C')
+else:
+	print('F') 
+
+
+
+
