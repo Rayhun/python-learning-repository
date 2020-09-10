e5bdde8e6d691ab30f97e212c580c034a9715e7f
rayhan <rayhunkhan27@gmail.com>
2020-09-10 18:17:27 +0600

    [if elif else]

--- if_elif_else.py
+++ if_elif_else.py
@@ -1,4 +1,4 @@
-name = 'Rayhan'
+# name = 'Rayhan'
 
 # if statement 
 
@@ -25,19 +25,102 @@ comment
 # else:
 # 	print('No')
 
-bangla = 47
+# bangla = 100
 
-if bangla >= 80:
-	print('A+')
-elif bangla >= 70:
-	print('A')
-elif bangla >= 60:
-	print('B')
-elif bangla >= 48:
-	print('C')
-else:
-	print('F') 
+# if bangla >= 80:
+# 	print('A+')
+# elif bangla >= 70:
+# 	print('A')
+# elif bangla >= 60:
+# 	print('B')
+# elif bangla >= 48:
+# 	print('C')
+# else:
+# 	print('F') 
+
+# car = 12
+# if car == 48:
+# 	print("Bus is full!")
+# elif car >= 24:
+# 	print("Bus is haf-full!")
+# else:
+# 	print("Bus is Empty")
 
 
+# Hidden Number is 3
+
+# find_number = 0
+# if find_number == 3:
+# 	print("Game Over!")
+# elif find_number == 2:
+# 	print("u can summition 1 +",find_number,"= 3" )
+# elif find_number == 1:
+# 	print("u can summition 2 +",find_number,"= 3")
+# else:
+# 	print("u can summition 3 +",find_number,"= 3")
 
 
+# py = 3
+# pa = 2
+# a = py == pa
+# print(a)
+
+# a = input("Enter Your Name: ")
+# b = input("Enter Your Father Name: ")
+# c = input("Enter Your Mother Name: ")
+# d = input("Enter Your SSC Section: ")
+# print("Your Personal Information:","\nName:" ,a,"\nFather Name:" ,b, ",\nMother Name: " ,c, ",\nCSS Section:",d)
+
+
+# num = 12
+# if num > 5:
+#     print("Bigger than 5")
+#     if num <= 47:
+#         print("Between 6 and 47")
+
+# any_number = 90
+# if any_number < 100:
+# 	print("1st Statement is Done!")
+# 	if any_number < 120:
+# 		print("2nd Number is Done!")
+# 		if any_number != 150:
+# 			print("3rd Number is Done!")
+# 		else:
+# 			print("3rd Number is failed!")
+# 	else:
+# 		print("2nd Number is Failed!")
+# else:
+# 	print("1st statement Failed")
+
+# number = 5
+# if number == 4:
+# 	print("Your Number is 4")
+# else:
+# 	if number == 3:
+# 		print("Your Number is 3")
+# 	else:
+# 		if number < 2:
+# 			print("Your Number is 5")
+# 		else:
+# 			print("Number not Found!!")
+
+
+# number = 2
+# if number == 5:
+#     print("Number is 5")
+# elif number == 11:
+#     print("Number is 11")
+# elif number == 7:
+#     print("Number is 7")
+# else:
+#     print("Number isn't 5, 11 or 7")
+
+# a = 1
+# b = "Logout" if a == 1 else "Login"
+# print(b)
+
+number = 2
+while number <= 10:
+   print(number)
+   number = number + 2
+print("I a Programmar in python .")
