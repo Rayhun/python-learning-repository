2fb7a794fc92a84a6ba7e85ac0ede23383e20776
rayhan <rayhunkhan27@gmail.com>
2020-09-12 01:06:55 +0600

    car game

---
+++ car-game.py
@@ -0,0 +1,29 @@
+def car_game():
+    chance = 3
+    primary = 0 
+    condition = False
+    while chance > primary:
+        inputted = str(input("Enter Your Command: ").lower())
+        primary += 1
+        if inputted == "start":
+            if condition:
+                print("car on the way")
+                break
+            else:
+                condition = True
+                print("car start")
+        elif inputted == "stop":
+            if condition:
+                print("car already stopped!")
+                break
+            else:
+                condition = True
+                print("car stop")
+        elif inputted == "help":
+            print('''
+                wright the "start" = car start,
+                wright the "stop" = car stop
+                ''')
+        else:
+            print("ask me only \"start\" or \"stop\" & \"help\"")
+car_game()
