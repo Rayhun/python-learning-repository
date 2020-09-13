780775a29086f0be3c5104bb259b3dd3bf613bb0
rayhan <rayhunkhan27@gmail.com>
2020-09-13 12:26:11 +0600

    daily routine

---
+++ routin.py
@@ -0,0 +1,22 @@
+for i in range(25):
+    inp = int(input("Enter Time: "))
+    if inp == 0:
+        print(" invalide", "\"Try ", i,  "Time\"")
+    elif inp <= 3:
+        print("Learning Python ", "\"Try ", i,  "Time\"")
+    elif inp <= 8:
+        print("Sleeping Time", "\"Try ", i,  "Time\"")
+    elif inp <= 9:
+        print("Brack first Time", "\"Try ", i,  "Time\"")
+    elif inp <= 13:
+        print("Web Design", "\"Try ", i,  "Time\"")
+    elif inp <= 15:
+        print("Lunch Time", "\"Try ", i,  "Time\"")
+    elif inp <= 19:
+        print("Learning Python Time", "\"Try ", i,  "Time\"" )
+    elif inp <= 21:
+        print("Refashioned ", "\"Try ", i,  "Time\"")
+    elif inp <= 23:
+        print("Dinner", "\"Try ", i,  "Time\"")
+    else:
+        print("Out of range")
--- while.py
+++ while.py
@@ -1,10 +1,28 @@
 ''' while loop '''
 
-i = 1 # loop starting value 
-summation = 0 # 15, 5
+# i = 1 # loop starting value 
+# summation = 0 # 15, 5
 
-while i < 5:
-	summation += i 
-	i = i + 1 # in, dec 
+# while i < 5:
+# 	summation += i 
+# 	i = i + 1 # in, dec 
 
-print(summation)
+# print(summation)
+
+st_num = 0
+# while st_num < 5:
+# 	print("bd cse solved")
+# 	st_num = st_num + 1 
+
+# while st_num < 5:
+# 	st_num = st_num + 1
+# 	if st_num == 4:
+# 		continue
+# 	print("rayhan" ,st_num)
+
+result = 0 #res 3
+num = 3 #num 4
+for i in range(2):
+    result = result + num
+    num = num + 1
+print(num)
